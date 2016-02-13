9e5e25dbfabb3792f00d5aaf38c2b1cea97f1c80
tatelucas <tate.lucas@gmail.com>
2016-02-12 20:59:51 -0500

    style changes

---
+++ js/themevast/growpro.js
@@ -0,0 +1,4 @@
+jQuery( document ).ready(function() {
+    //console.log(jQuery("#product-options-wrapper").html());
+    jQuery("#product-options-wrapper label:contains('Custom Text')").html("Notes/Info: <br /><span>Please give us any additional information or instruction for your product. Keep in mind that you will see a proof before printing is done</span>");
+});
